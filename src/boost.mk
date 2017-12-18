ff22cbf8606325fb631072f7f48c1baccad8c4a3
Administrator <nedobryj@icloud.com>
2017-12-18 19:23:18 +0300

    boost fix

--- src/boost.mk
+++ src/boost.mk
@@ -1,53 +1,39 @@
-# This file is part of MXE. See LICENSE.md for licensing information.
+# This file is part of MXE.
+# See index.html for further information.
 
 PKG             := boost
-$(PKG)_WEBSITE  := https://www.boost.org/
-$(PKG)_DESCR    := Boost C++ Library
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.60.0
-$(PKG)_CHECKSUM := 686affff989ac2488f79a97b9479efb9f2abae035b5ed4d8226de6857933fd3b
+$(PKG)_VERSION  := 1.53.0
+$(PKG)_CHECKSUM := e6dd1b62ceed0a51add3dda6f3fc3ce0f636a7f3
 $(PKG)_SUBDIR   := boost_$(subst .,_,$($(PKG)_VERSION))
 $(PKG)_FILE     := boost_$(subst .,_,$($(PKG)_VERSION)).tar.bz2
-$(PKG)_URL      := https://$(SOURCEFORGE_MIRROR)/project/boost/boost/$($(PKG)_VERSION)/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc bzip2 expat zlib
+# https://sourceforge.net/projects/boost/files/boost/1.53.0/boost_1_53_0.tar.bz2
+$(PKG)_URL      := http://sourceforge.net/project/boost/files/boost/$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc zlib bzip2 expat
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://www.boost.org/users/download/' | \
+    $(WGET) -q -O- 'http://www.boost.org/users/download/' | \
     $(SED) -n 's,.*/boost/\([0-9][^"/]*\)/".*,\1,p' | \
     grep -v beta | \
     head -1
 endef
 
 define $(PKG)_BUILD
+    # context switched library introduced in boost 1.51.0 does not build
+    rm -r '$(1)/libs/context'
     # old version appears to interfere
     rm -rf '$(PREFIX)/$(TARGET)/include/boost/'
-    rm -f "$(PREFIX)/$(TARGET)/lib/libboost"*
-
-    # create user-config
-    echo 'using gcc : mxe : $(TARGET)-g++ : <rc>$(TARGET)-windres <archiver>$(TARGET)-ar <ranlib>$(TARGET)-ranlib ;' > '$(1)/user-config.jam'
-
-    # compile boost build (b2)
-    cd '$(1)/tools/build/' && ./bootstrap.sh
-
-    # cross-build, see b2 options at:
-    # https://www.boost.org/build/doc/html/bbv2/overview/invocation.html
-    cd '$(1)' && ./tools/build/b2 \
-        -a \
-        -q \
+    echo 'using gcc : : $(TARGET)-g++ : <rc>$(TARGET)-windres <archiver>$(TARGET)-ar <ranlib>$(TARGET)-ranlib ;' > '$(1)/user-config.jam'
+    # compile boost jam
+    cd '$(1)/tools/build/v2/engine' && ./build.sh
+    cd '$(1)' && tools/build/v2/engine/bin.*/bjam \
         -j '$(JOBS)' \
         --ignore-site-config \
         --user-config=user-config.jam \
-        abi=ms \
-        address-model=$(BITS) \
-        architecture=x86 \
-        binary-format=pe \
-        link=$(if $(BUILD_STATIC),static,shared) \
         target-os=windows \
-        threadapi=win32 \
         threading=multi \
-        variant=release \
-        toolset=gcc-mxe \
-        cxxflags=$(if $(findstring posix,$(MXE_GCC_THREADS)),-std=gnu++11,-std=gnu++98) \
+        link=$(if $(BUILD_STATIC),static,shared) \
+        threadapi=win32 \
         --layout=tagged \
         --disable-icu \
         --without-mpi \
@@ -58,29 +44,17 @@ define $(PKG)_BUILD
         --includedir='$(PREFIX)/$(TARGET)/include' \
         -sEXPAT_INCLUDE='$(PREFIX)/$(TARGET)/include' \
         -sEXPAT_LIBPATH='$(PREFIX)/$(TARGET)/lib' \
-        install
+        stage install
 
     $(if $(BUILD_SHARED), \
         mv -fv '$(PREFIX)/$(TARGET)/lib/'libboost_*.dll '$(PREFIX)/$(TARGET)/bin/')
 
-    # setup cmake toolchain
-    echo 'set(Boost_THREADAPI "win32")' > '$(CMAKE_TOOLCHAIN_DIR)/$(PKG).cmake'
-
     '$(TARGET)-g++' \
         -W -Wall -Werror -ansi -U__STRICT_ANSI__ -pedantic \
-        '$(PWD)/src/$(PKG)-test.cpp' -o '$(PREFIX)/$(TARGET)/bin/test-boost.exe' \
+        '$(2).cpp' -o '$(PREFIX)/$(TARGET)/bin/test-boost.exe' \
         -DBOOST_THREAD_USE_LIB \
         -lboost_serialization-mt \
         -lboost_thread_win32-mt \
         -lboost_system-mt \
-        -lboost_chrono-mt \
-        -lboost_context-mt
-
-    # test cmake
-    mkdir '$(1).test-cmake'
-    cd '$(1).test-cmake' && '$(TARGET)-cmake' \
-        -DPKG=$(PKG) \
-        -DPKG_VERSION=$($(PKG)_VERSION) \
-        '$(PWD)/src/cmake/test'
-    $(MAKE) -C '$(1).test-cmake' -j 1 install
+        -lboost_chrono-mt
 endef
